d93d36298ca1a87682d8d48e4d2a241ce7167b6a
Yuval Lerner <uvluni@gmail.com>
2024-01-01 08:46:38 +0200

    moved upload logic from server.js to uploadHandler.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
-
 const { getLocations } = require('./locationHandler');
 const { sendFile } = require('./fileProcessor');
+const { upload } = require('./uploadHandler');
 
 const app = express();
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, 'uploads'));
-    },
-    filename: function (req, file, cb) {
-      const date = new Date();
-      const formattedDate = `${('0' + date.getDate()).slice(-2)}_${('0' + (date.getMonth() + 1)).slice(-2)}_${date.getFullYear()}-${('0' + date.getHours()).slice(-2)}_${('0' + date.getMinutes()).slice(-2)}`;
-      const fileName = file.originalname.split('.')[0];
-      const fileExtension = file.originalname.slice(file.originalname.lastIndexOf('.'));
-      cb(null, `${fileName}-${formattedDate}${fileExtension}`);
-    }
-  })
-});
 
 // Handle CORS if your frontend and backend are on different origins
 app.use((req, res, next) => {
